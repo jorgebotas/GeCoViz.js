2f9bbea49975c8acbccff95b1a2b45ac03a0cdae
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-02-15 11:51:47 +0100

    domains

--- popper.js
+++ popper.js
@@ -150,7 +150,8 @@ var PopperCreate = function(selector, d, URLs) {
         console.log(d.end)
         draw_protDomains(selector + ' #dom' + cleanString(d.anchor + d.pos),
                          d.domains,
-                         Math.abs((+d.end) - (+d.start)),
+                         1000,
+                         //Math.abs((+d.end) - (+d.start)),
                          250,
                          7,
                          palette,
