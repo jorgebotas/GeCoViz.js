f8c8e26e793d3632c76f275425f6e13ab9023eea
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-03-11 11:10:22 +0100

    urls

--- src/customBar.js
+++ src/customBar.js
@@ -51,7 +51,7 @@ var customBar = function(selector, data) {
     createSlider(nSideSlider,
         'nSideSlider',
         options = {
-            start : 2,
+            start : 10,
             step : 1,
             min : 0,
             max : 10
