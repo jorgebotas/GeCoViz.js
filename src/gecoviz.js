cfb74bfedc4c07e72f8646740bdb51a6e4a88a06
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-03-15 13:40:50 +0100

    hiddenKeys

--- src/gecoviz.js
+++ src/gecoviz.js
@@ -1165,7 +1165,8 @@ var GeCoViz = function(selector) {
               d.vStart = undefined;
               d.vEnd = undefined;
           }
-          console.log(`start ${d.vStart}  end${d.vEnd}`)
+          console.log(`start ${d.vStart}  end ${d.vEnd}`)
+          console.log(`start ${d.start}  end ${d.end}`)
       })
   }
 
